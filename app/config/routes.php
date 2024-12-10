caa1408518a517f58701970396e882f43fc13de5
BernardoLF <bernardolfontes20@gmail.com>
2024-12-10 14:18:26 +0000

    uodate routes, add Envent View e Controller

--- app/config/routes.php
+++ app/config/routes.php
@@ -8,7 +8,27 @@ return[
     'login' => [
         'controller' => 'AuthController',
         'action' => 'login'
-    ]
+    ],
+    'dashboard' => [
+        'controller' => 'dashboardController',
+        'action' => ''
+    ],
+    'Event' => [
+        'controller' => 'EventController',
+        'action' => ''
+    ],
+    'NewEvent' => [
+        'controller' => 'NewEventController',
+        'action' => ''
+    ],
+    'Perfil' => [
+        'controller' => 'perfilController',
+        'action' => ''
+    ],
+    'UserList' => [
+        'controller' => 'UserListController',
+        'action' => ''
+    ],
 ]
 
 ?>
